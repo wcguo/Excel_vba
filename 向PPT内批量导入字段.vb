44626455b7c478b40c880a7104c992fc70e85e00
wcguo <35400769+wcguo@users.noreply.github.com>
2022-05-09 10:03:37 +0800

    Update 向PPT内批量导入字段.vb

--- 向PPT内批量导入字段.vb
+++ 向PPT内批量导入字段.vb
@@ -10,10 +10,10 @@ Sub 批量导出重复格式内容()
     
     Set pptPre = ActivePresentation
     
-    myPath = "C:\Users\5600X\Desktop\123\xl\media\" '图片位置
+    myPath = "C:\Users\xl\media\" '图片位置
     
     Set appExcel = CreateObject("Excel.Application") '创建excel对象
-    Set myexcel = appExcel.Workbooks.Open("C:\Users\5600X\Desktop\123.xlsx") '打开工作表
+    Set myexcel = appExcel.Workbooks.Open("C:\123.xlsx") '打开工作表
     Set mysheet = myexcel.sheets("Sheet1") '创建工作表对象
     rcount = mysheet.Cells(mysheet.Rows.Count, "A").End(3).Row '获取工作表最后一行行号
     
